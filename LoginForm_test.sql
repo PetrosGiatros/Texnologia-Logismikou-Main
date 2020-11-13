a305a17e550392b76f6e7069a71d0508344bd502
AndGeo69 <73225938+AndGeo69@users.noreply.github.com>
2020-11-13 17:47:56 +0200

    Create teh databse updated
    prosthikh username.

--- LoginForm_test.sql
+++ LoginForm_test.sql
@@ -1,10 +1,10 @@
 create database loginform_test; /* create the database with name loginform_test */
 use loginform_test; /* we use this sql statement for making database loginform_test the current database */
-create table employer ( email varchar(20), password varchar(20)); /* we use this sql statement for creating a table in database with the name employer. This table will have two columns the email and password */
+create table employer (username varchar(30), email varchar(30), password varchar(30)); /* we use this sql statement for creating a table in database with the name employer. This table will have two columns the email and password */
 show tables; /* we use this sql statement to see the tables in our database */
 describe employer; /* we use this sql statement to verify that our table was created with the way we expected */
 insert into employer
-	values ('kitsos@kitsos.com', 'Kitsos1'); /* we use this sql statement to load data into our table */
+	values ('kitsos','kitsos@kitsos.com', 'Kitsos1'); /* we use this sql statement to load data into our table */
 insert into employer
-	values ('mhtsos@mhtsos.com', 'Mhtsos1');
+	values ('mhtsos','mhtsos@mhtsos.com', 'Mhtsos1');
 select * from employer /* we use this statement to retrieve all the data from the table */

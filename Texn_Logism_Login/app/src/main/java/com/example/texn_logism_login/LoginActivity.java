59c90ac856964f90d2d911643b8a0c5d21b95844
AndGeo69 <73225938+AndGeo69@users.noreply.github.com>
2020-11-16 17:36:33 +0200

    Updated Login Stable version
    Connect to DB with 000webhosting.com
    via PHP scripts

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/LoginActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/LoginActivity.java
@@ -2,19 +2,30 @@ package com.example.texn_logism_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
-        private TextView usernameTextView, passwordTextView;
-        private Button loginButton;
+    private TextView usernameTextView, passwordTextView, status;
+    private Button loginButton;
 
+    String finalResult ;
+    boolean CheckEditText;
+    String usernameHolder,passwordHolder;
+    ProgressDialog progressDialog;
+    HashMap<String,String> hashMap = new HashMap<>();
+    HttpParse httpParse = new HttpParse();
 
+    String HttpURL = "http://priapic-blower.000webhostapp.com/userlogin.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,40 +33,77 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginButton = (Button) findViewById(R.id.buttonLogin);
-        usernameTextView= (TextView) findViewById(R.id.editTextUsername);
+        usernameTextView = (TextView) findViewById(R.id.editTextUsername);
         passwordTextView = (TextView) findViewById(R.id.editTextTextPassword);
-
-        LoginBean loginbean = new LoginBean();
-        LoginDao dao = new LoginDao();
-
+        status = (TextView) findViewById(R.id.status);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
-                    loginbean.setUsername(String.valueOf(usernameTextView.getText()));
-                    loginbean.setPassword(String.valueOf(passwordTextView.getText()));
 
-                    String userValid = dao.authEmployer(loginbean);
-                    if(userValid=="Success"){
-                        System.out.println("Egine Validation");
-                    }
-                    else if(userValid=="Invalid Credentials"){
-                        System.out.println("Lathos stoixeia, prospathistes jana.");
-                    }
+                String username = usernameTextView.getText().toString();
+                String password = passwordTextView.getText().toString();
 
-                } catch (Exception e) {
-                    e.printStackTrace();
+                CheckEditTextIsEmptyOrNot();
+                if(CheckEditText){
+                    UserLoginFunction(username,password);
                 }
-
-
+                else{
+                    Toast.makeText(LoginActivity.this,"Please fill form fields.",Toast.LENGTH_LONG).show();
+                }
             }
+
         });
 
+    }
+        public void CheckEditTextIsEmptyOrNot(){
+                 usernameHolder = usernameTextView.getText().toString();
+                 passwordHolder = passwordTextView.getText().toString();
+                if ((TextUtils.isEmpty(usernameHolder)) || (TextUtils.isEmpty(passwordHolder))) {
+                    CheckEditText = false;
+                } else {
+                    CheckEditText = true;
+                }
+            }
+
+    public void UserLoginFunction(String username,String password){
+        class UserLoginClass extends AsyncTask<String,Void,String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = ProgressDialog.show(LoginActivity.this,"Loading Data",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+                super.onPostExecute(httpResponseMsg);
+                progressDialog.dismiss();
+                if(httpResponseMsg.equalsIgnoreCase("Data Matched")) {
+                    Toast.makeText(LoginActivity.this,httpResponseMsg,Toast.LENGTH_LONG).show();
+                    status.setText("Login Successfully.");
+
+                    finish();
+                }
+                else{
+                    status.setText("Login Failed.");
+                    System.out.println("Error sto onPostExecute");
+                    Toast.makeText(LoginActivity.this,httpResponseMsg,Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            protected String doInBackground(String... params) {
+
+                hashMap.put("username",params[0]);
+                hashMap.put("password",params[1]);
+                finalResult = httpParse.postRequest(hashMap, HttpURL);
+                return finalResult;
+            }
+        }
+        UserLoginClass userLoginClass = new UserLoginClass();
+        userLoginClass.execute(username,password);
+        }
 
     }
 
 
 
-
-}

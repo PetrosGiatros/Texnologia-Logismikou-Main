96533af596f7aaa1218adda0eac548fae7a6180b
AndGeo69 <73225938+AndGeo69@users.noreply.github.com>
2020-11-27 22:19:33 +0200

    updated

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/CreateActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/CreateActivity.java
@@ -1,8 +1,38 @@
 package com.example.texn_logism_login;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class CreateActivity  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_form);
+        Spinner spinnerScheduleType = findViewById(R.id.spinnerScheduleType);
+        Spinner spinnerShiftType = findViewById(R.id.spinnerShiftType);
+        Spinner spinnerProfession = findViewById(R.id.spinnerProfession);
+
+        String[] ScheduleTypes = new String[]{"Weekly", "Monthly", "Trimestser","Semster"};
+        String[] ShiftTypes = new String[]{"8", "4"};
+        String[] Profession = new String[]{"Programmer", "Analyst","Manager"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, ScheduleTypes);
+        spinnerScheduleType.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, ShiftTypes);
+        spinnerShiftType.setAdapter(adapter2);
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Profession);
+        spinnerProfession.setAdapter(adapter3);
+
+    }
+
+
+
 
 
 }

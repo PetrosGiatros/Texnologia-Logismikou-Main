56ec12ad869e2e2ee992ac5f22e544cdc19ef1a9
kostastsif <73225991+kostastsif@users.noreply.github.com>
2020-11-29 18:30:06 +0200

    php for the database connection with the calendar

---
+++ getSchedule.php
@@ -0,0 +1,39 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+  require 'init.php';
+  $user_name = $_POST["loggedinUsername"];
+
+  
+  $sql = "select ID from login where username = '$user_name' ";
+    
+  $result = mysqli_query($con,$sql);
+  $row=mysqli_fetch_array($result);
+
+  $employerID = $row[0];
+  
+  $sql2="select username,ID from login where isEmployedTo = '$employerID'";
+  
+  $result2=mysqli_query($con,$sql2);
+  $row2=mysqli_fetch_array($result2);
+ 
+ echo $row2['username']." ";
+ echo $row2['ID']." ";
+
+ while($row2= $result2->fetch_array())
+   {
+       echo $row2['username']." ".$row2['ID']." ";
+   }
+  
+  
+ # if(!mysqli_num_rows($result2)>0){
+ #   echo "Error";
+ # }
+ # else{
+ #  echo $row2['username'];
+ # }
+}
+
+mysqli_close($con);
+
+?>

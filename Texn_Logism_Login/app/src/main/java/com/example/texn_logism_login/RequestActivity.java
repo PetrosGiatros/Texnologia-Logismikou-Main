3d53ae3bc6d6b814174536465ab85e1f48fe1dfe
EmmaTsel <tselios2014@gmail.com>
2020-12-04 23:42:03 +0200

    new function

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
@@ -1,17 +1,30 @@
 package com.example.texn_logism_login;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 public class RequestActivity extends AppCompatActivity
 {
-    String HttpURL = "http://priapic-blower.000webhostapp.com/getRequest.php";
+
     private Button submitLeaveRequestButton;
     private EditText leaveDays;
+
+    String HttpURL = "http://priapic-blower.000webhostapp.com/getRequest.php";
+    String finalResult ;
+    HttpParse httpParse = new HttpParse();
+    HashMap<String,String> dataMap = new HashMap<>();
+    static String[] employeeArray={};
+    ArrayAdapter adapter;
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -30,5 +43,54 @@ public class RequestActivity extends AppCompatActivity
             }
         });
 
+    }
+
+    public String[] getIDInfo(String loggedInUsername, ListView listView){
+
+        class GetIDInfoClass extends AsyncTask<String,Void,String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+                super.onPostExecute(httpResponseMsg);
+
+
+                int j =0;
+                for(int i =0;i<employeeArray.length;i=i+2){
+                    employeeArray[j]=employeeArray[i] +" "+employeeArray[i+1];
+                    j=j+1;
+                }
+                int temp=j;
+                for(int i=temp;i<employeeArray.length;i++){
+                    employeeArray[i]="";
+                }
+                String[] employeeArrayNew=new String[employeeArray.length/2];
+                for(int i=0;i<temp;i++){
+                    employeeArrayNew[i]=employeeArray[i];
+                }
+
+                adapter=new ArrayAdapter<String>(getApplicationContext(),R.layout.activity_listview,R.id.label, employeeArrayNew);
+                listView.setAdapter(adapter);
+
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                dataMap.put("loggedinUsername",params[0]);
+                System.out.println(params[0]);
+                finalResult = httpParse.postRequest(dataMap, HttpURL);
+                employeeArray = finalResult.split(" ");
+
+                dataMap.put("employees",finalResult);
+                System.out.println("result: "+finalResult);
+                return finalResult;
+            }
+        }
+        GetIDInfoClass getIDInfoClass = new GetIDInfoClass();
+        getIDInfoClass.execute(loggedInUsername);
+        return employeeArray;
     }
 }

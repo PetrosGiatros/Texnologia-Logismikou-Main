dd939f84899f208583942b00b56ef5e4c35dbe79
PolarityFive <39606152+PolarityFive@users.noreply.github.com>
2020-12-04 21:01:53 +0200

    OnClick

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/NotificationsActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/NotificationsActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationsActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications_form);
 
--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
@@ -1,15 +1,30 @@
 package com.example.texn_logism_login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RequestActivity extends AppCompatActivity
 {
-
-    protected void onCreate(Bundle savedInstanceState) {
+    private Button submitLeaveRequestButton;
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.requests_form);
+        submitLeaveRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+
+
+
+
+
+            }
+        });
 
     }
 }

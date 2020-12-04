0114ead4d6501d807dd043bd095f2545443f1187
PolarityFive <39606152+PolarityFive@users.noreply.github.com>
2020-12-04 21:41:39 +0200

    We got the days.

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
@@ -3,26 +3,28 @@ package com.example.texn_logism_login;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RequestActivity extends AppCompatActivity
 {
+    //String HttpURL = "http://priapic-blower.000webhostapp.com/getRequest.php";
     private Button submitLeaveRequestButton;
+    private EditText leaveDays;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.requests_form);
+        submitLeaveRequestButton=(Button)findViewById(R.id.leaveButton);
+        EditText leaveDays = (EditText)findViewById(R.id.leaveDays);
+
         submitLeaveRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-
-
-
-
-
-
+                int number = Integer.parseInt(leaveDays.getText().toString());
+                System.out.println("Leave Days: " + number);
             }
         });
 

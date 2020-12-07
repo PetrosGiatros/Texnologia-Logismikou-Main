0fba977ef147e28a8b839441193c95d691c2c058
PolarityFive <39606152+PolarityFive@users.noreply.github.com>
2020-12-07 23:30:27 +0200

    Connected the request/notification forms.
    They're ugly.... but they're connected.

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/NotificationsActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/NotificationsActivity.java
@@ -3,18 +3,32 @@ package com.example.texn_logism_login;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class NotificationsActivity extends AppCompatActivity {
+    static String[] employeeArray={};
+    ArrayAdapter adapter;
+    ListView listView;
+
     String finalResult;
     HashMap<String, String> hashMap = new HashMap<>();
     HttpParse httpParse = new HttpParse();
+
+
     String HttpURL = "http://priapic-blower.000webhostapp.com/setNotifications.php";
     String loggedInUsername = LoginActivity.getUsernameTextView().getText().toString();
-    public void setFunction (String loggedInUsername){
+
+
+    public void setFunction (String loggedInUsername, ListView listView){
         class setClass extends AsyncTask<String, Void, String> {
             @Override
             protected void onPreExecute() {
@@ -26,16 +40,33 @@ public class NotificationsActivity extends AppCompatActivity {
                 super.onPostExecute(httpResponseMsg);
                 System.out.println(httpResponseMsg);
                 int k = 0;
-                String[] returnedString = httpResponseMsg.split(" ");
-                String[][] returnedEmployeesFinal = new String[returnedString.length/4][4];
-                for (int i = 0; i < returnedString.length/4; i++)
+                employeeArray = httpResponseMsg.split(" ");
+                String[][] returnedEmployeesFinal = new String[employeeArray.length/4][4];
+                ListView list;
+                for (int i = 0; i < employeeArray.length/4; i++)
                 {
                     for (int j = 0; j < 4; j++)
                     {
-                        returnedEmployeesFinal[i][j] = returnedString[k];
+                        returnedEmployeesFinal[i][j] = employeeArray[k];
                         k++;
                     }
                 }
+                int j =0;
+                for(int i = 0;i<employeeArray.length;i = i + 4){
+                    employeeArray[j]=employeeArray[i] +" "+employeeArray[i+1]+" "+employeeArray[i+2]+" "+employeeArray[i+3];
+                    j=j+1;
+                }
+                int temp=j;
+                for(int i=temp;i<employeeArray.length;i++){
+                    employeeArray[i]="";
+                }
+                String[] employeeArrayNew=new String[employeeArray.length/4];
+                for(int i=0;i<temp;i++){
+                    employeeArrayNew[i]=employeeArray[i];
+                }
+
+                adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.activity_listview,R.id.label, employeeArray);
+                listView.setAdapter(adapter);
             }
 
 
@@ -51,8 +82,10 @@ public class NotificationsActivity extends AppCompatActivity {
     }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.notifications_form);
-        setFunction(loggedInUsername);
+        ListView listView = (ListView) findViewById(R.id.notificationsListView);
+        setFunction(loggedInUsername,listView);
         System.out.println("Logged in username " + loggedInUsername);
     }
 }

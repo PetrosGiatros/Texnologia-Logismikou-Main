14bf08444a0f47678afd5bf934e1985d0974636d
EmmaTsel <tselios2014@gmail.com>
2020-12-08 17:10:18 +0200

    Revert "Updates and new utility"
    This reverts commit b9437ac5d21c2b61147f7f458abe2fd9b4763664.

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/CreateActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/CreateActivity.java
@@ -256,17 +256,16 @@ public class CreateActivity  extends AppCompatActivity {
                 System.out.println(); //Makes a new row
             }
 
-            //util.displaySchedule(users,schedule,employeeAmountPerShift,Integer.valueOf(SelectedShiftType)*type*numOfShifts,Integer.valueOf(SelectedShiftType));
-            util.saveSchedule(users,schedule,employeeAmountPerShift,Integer.valueOf(SelectedShiftType)*type*numOfShifts,Integer.valueOf(SelectedShiftType),numOfShifts);
+            util.displaySchedule(users,schedule,employeeAmountPerShift,Integer.valueOf(SelectedShiftType)*type*numOfShifts,Integer.valueOf(SelectedShiftType));
         }
 
 
 
-        /*for(int i =0;i<users.length;i++){
+        for(int i =0;i<users.length;i++){
 
             System.out.println("O user: "+users[i].FirstName+" exei "+users[i].totalHours+" wres left. Kai exei doulepsei "+sumHoursPerEmployee[i]+" fores.");
 
-        }*/
+        }
 
 
     }

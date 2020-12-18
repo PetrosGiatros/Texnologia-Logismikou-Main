db108d7aa8158f0b26509ccbbfc1e4e58a8c9bc7
AndGeo69 <73225938+AndGeo69@users.noreply.github.com>
2020-12-18 18:41:51 +0200

    Updated request form
    new getUserWorkingDates.php that retrieve the working days from the logged in user and apply them to spinner.

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
@@ -5,21 +5,28 @@ package com.example.texn_logism_login;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.HashMap;
 
 
 public class RequestActivity extends AppCompatActivity
 {
-    private Button submitLeaveRequestButton;
+    private Button submitLeaveRequestButton,BackButton;
     private EditText leaveDays;
+    private Spinner spinnerWorkDates;
 
     String finalResult ;
     HashMap<String,String> hashMap = new HashMap<>();
     HttpParse httpParse = new HttpParse();
     String HttpURL = "http://priapic-blower.000webhostapp.com/getRequest.php";
+    String HttpURL2 = "http://priapic-blower.000webhostapp.com/getUserWorkingDates.php";
+    String[] workingDates={};
 
 
     public void leaveFunction(String username,String days) {
@@ -48,14 +55,45 @@ public class RequestActivity extends AppCompatActivity
     }
 
 
+    public void userWorkingDates(String username) {
+        class userWorkingDatesClass extends AsyncTask<String, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+                super.onPostExecute(httpResponseMsg);
+                System.out.println(httpResponseMsg);
+                spinnerWorkDates.setAdapter(new ArrayAdapter<String>(RequestActivity.this, android.R.layout.simple_spinner_dropdown_item, workingDates));
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                hashMap.put("username", params[0]);
+                finalResult = httpParse.postRequest(hashMap, HttpURL2);
+                workingDates=finalResult.split(" ");
+                return finalResult;
+            }
+        }
+        userWorkingDatesClass userWorkObj = new userWorkingDatesClass();
+        userWorkObj.execute(username);
+    }
+
+
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.requests_form);
+        BackButton = (Button)findViewById(R.id.buttonBack);
         submitLeaveRequestButton=(Button)findViewById(R.id.leaveButton);
         EditText leaveDays = (EditText)findViewById(R.id.leaveDays);
         String loggedInUsername = LoginActivity.getUsernameTextView().getText().toString();
+        spinnerWorkDates=(Spinner)findViewById(R.id.spinnerWorkDates);
+        userWorkingDates(loggedInUsername);
+
+
 
 
         submitLeaveRequestButton.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +104,17 @@ public class RequestActivity extends AppCompatActivity
                 leaveFunction(loggedInUsername,requestedLeaveDays);
             }
         });
+
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(RequestActivity.this,"It's Not Working Yet, so sad :(.",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
 
     }
 

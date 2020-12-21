562c2e66467094136f23ee5e05790d113df73b1a
PolarityFive <39606152+PolarityFive@users.noreply.github.com>
2020-12-21 22:18:15 +0200

    Fixed Master. Sort of.

--- Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
+++ Texn_Logism_Login/app/src/main/java/com/example/texn_logism_login/RequestActivity.java
@@ -4,22 +4,41 @@ package com.example.texn_logism_login;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 
 
 public class RequestActivity extends AppCompatActivity
 {
-    private Button submitLeaveRequestButton;
-    private EditText leaveDays;
+    private Button submitLeaveRequestButton,BackButton;
+    private TextView leaveDays;
+    private Spinner spinnerWorkDates;
 
-    String finalResult ;
+    String finalResult ,finalresult2;
     HashMap<String,String> hashMap = new HashMap<>();
+    HashMap<String,String> hashMap2 = new HashMap<>();
     HttpParse httpParse = new HttpParse();
     String HttpURL = "http://priapic-blower.000webhostapp.com/getRequest.php";
+    String HttpURL2 = "http://priapic-blower.000webhostapp.com/getUserWorkingDates.php";
+    String[] workingDates={};
+    String startingDate,leaveDayHolder;
+    Object startingDate2;
+    boolean CheckEditText;
+
+
+
 
 
 
@@ -28,8 +47,6 @@ public class RequestActivity extends AppCompatActivity
 
 
     public void leaveFunction(String username,String days,String startingDate) {
-
-
         class leaveClass extends AsyncTask<String, Void, String> {
             @Override
             protected void onPreExecute() {
@@ -46,12 +63,48 @@ public class RequestActivity extends AppCompatActivity
             protected String doInBackground(String... params) {
                 hashMap.put("username", params[0]);
                 hashMap.put("leaveDays", params[1]);
-                finalResult = httpParse.postRequest(hashMap, HttpURL);
-                return finalResult;
+                hashMap.put("startingDate", params[2]);
+                finalresult2 = httpParse.postRequest(hashMap, HttpURL);
+
+                System.out.println("apotelesmata php: "+finalresult2);
+                //checkLeaveFunction(finalResult);
+
+                return finalresult2;
             }
         }
         leaveClass leaveObject = new leaveClass();
-        leaveObject.execute(username,days);
+        leaveObject.execute(username,days,startingDate);
+
+
+    }
+
+
+
+
+
+    public void userWorkingDates(String username) {
+        class userWorkingDatesClass extends AsyncTask<String, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+                super.onPostExecute(httpResponseMsg);
+                System.out.println(httpResponseMsg);
+                spinnerWorkDates.setAdapter(new ArrayAdapter<String>(RequestActivity.this, android.R.layout.simple_spinner_dropdown_item, workingDates));
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                hashMap.put("username", params[0]);
+                finalResult = httpParse.postRequest(hashMap, HttpURL2);
+                workingDates=finalResult.split(" ");
+                return finalResult;
+            }
+        }
+        userWorkingDatesClass userWorkObj = new userWorkingDatesClass();
+        userWorkObj.execute(username);
     }
 
 
@@ -60,20 +113,66 @@ public class RequestActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.requests_form);
+        BackButton = (Button)findViewById(R.id.buttonBack);
         submitLeaveRequestButton=(Button)findViewById(R.id.leaveButton);
-        EditText leaveDays = (EditText)findViewById(R.id.leaveDays);
+        leaveDays = (TextView)findViewById(R.id.leaveDays);
         String loggedInUsername = LoginActivity.getUsernameTextView().getText().toString();
+        spinnerWorkDates=(Spinner)findViewById(R.id.spinnerWorkDates);
+        userWorkingDates(loggedInUsername);
+
+
 
 
         submitLeaveRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
+
                 String requestedLeaveDays = leaveDays.getText().toString();
-                leaveFunction(loggedInUsername,requestedLeaveDays);
+                startingDate2= spinnerWorkDates.getSelectedItem();
+
+                CheckEditTextIsEmptyOrNot();
+                if(CheckEditText){
+                    if(startingDate2==null){
+                        Toast.makeText(RequestActivity.this,"You are not working yet.",Toast.LENGTH_LONG).show();
+                    }else{
+                        startingDate= spinnerWorkDates.getSelectedItem().toString();
+                        leaveFunction(loggedInUsername,requestedLeaveDays,startingDate);
+                    }
+                }
+                else{
+                    Toast.makeText(RequestActivity.this,"Please fill Days fields.",Toast.LENGTH_LONG).show();
+                }
+
+
+
+
             }
         });
 
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(RequestActivity.this,"It's Not Working Yet, so sad :(.",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
     }
+
+
+
+    public void CheckEditTextIsEmptyOrNot(){
+        leaveDayHolder = leaveDays.getText().toString();
+        if ((TextUtils.isEmpty(leaveDayHolder))) {
+            CheckEditText = false;
+        } else {
+            CheckEditText = true;
+        }
+    }
+
 
 }
